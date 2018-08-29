7f6d24c1435883b2e570859493ce07849e2c00f1
jtb1137 <jtb1137@gmail.com>
2018-08-29 13:51:29 -0400

    create new post route and post validations

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-// @route GET api/posts/test
-// @desc Tests post route
-// @access Public
+const validatePostInput = require("../../validation/post");
+
+const Post = require("../../models/Post");
+
+// @route   GET api/posts/test
+// @desc    Tests post route
+// @access  Public
 router.get("/test", (req, res) => {
   res.json({ message: "Posts functional" });
 });
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isEmpty } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    newPost
+      .save()
+      .then(post => res.json(post))
+      .catch(err => res.json(err));
+  }
+);
 
 module.exports = router;
