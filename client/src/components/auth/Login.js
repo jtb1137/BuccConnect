47262b1a331c62e70e5511646893d631864a3bf9
jtb1137 <jtb1137@gmail.com>
2018-08-30 13:00:08 -0400

    add error messages to Register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      errors: {},
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+
+    const newUser = {
+      email: this.state.email,
+      password: this.state.password,
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h2 className="display-4 text-center">Log In</h2>
+            <p className="lead text-center">Log into your BuccConnect account.</p>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <input type="email" className="form-control form-control-lg" placeholder="Email" name="email" value={this.state.email} onChange={this.onChange} />
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
+              </div>
+              <input type="submit" value="Submit" className="btn btn-block" />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
