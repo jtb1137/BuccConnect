a32c7fd5cc755277d70e144c1b1e41aaa441d3c2
jtb1137 <jtb1137@gmail.com>
2018-08-30 12:25:55 -0400

    build basic registration functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
 
 class Register extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {},
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Register</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h2 className="display-4 text-center">Sign Up</h2>
+            <p className="lead text-center">Create your BuccConnect account.</p>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange} />
+              </div>
+              <div className="form-group">
+                <input type="email" className="form-control form-control-lg" placeholder="Email" name="email" value={this.state.email} onChange={this.onChange} />
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange} />
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
+              </div>
+              <input type="submit" value="Submit" className="btn btn-block" />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
