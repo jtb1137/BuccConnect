2eaf9b8ce6fd22ca3c18d1224d3927d548b548a2
jtb1137 <jtb1137@gmail.com>
2018-09-04 15:56:30 -0400

    implement Profiles component

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Loading from "../shared/Loading";
+import { getProfiles } from "../../actions/profileActions";
+import isEmpty from "../../validation/is-empty";
+
+class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+  render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
+
+    if (isEmpty(profiles) || loading) {
+      profileItems = <Loading />;
+    } else {
+      if (profiles.length > 0) {
+        profileItems = <h1>Profiles listed here</h1>;
+      } else {
+        profileItems = <h4>No profiles found</h4>;
+      }
+    }
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="display-4 text-center">Profiles</h1>
+            {profileItems}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
