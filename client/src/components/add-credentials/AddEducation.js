5055271b90b9d908becc94037bae31053d20a8e9
jtb1137 <jtb1137@gmail.com>
2018-09-04 14:14:57 -0400

    implement AddEducation

---
+++ client/src/components/add-credentials/AddEducation.js
@@ -0,0 +1,150 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import TextFieldGroup from "../shared/TextFieldGroup";
+import TextAreaFieldGroup from "../shared/TextAreaFieldGroup";
+import { addEducation } from "../../actions/profileActions";
+
+class AddEducation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      school: "",
+      degree: "",
+      fieldofstudy: "",
+      startdate: "",
+      enddate: "",
+      current: false,
+      description: "",
+      errors: {},
+      disabled: false
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const eduData = {
+      school: this.state.school,
+      degree: this.state.degree,
+      fieldofstudy: this.state.fieldofstudy,
+      startdate: this.state.startdate,
+      enddate: this.state.enddate,
+      current: this.state.current,
+      description: this.state.description
+    };
+
+    this.props.addEducation(eduData, this.props.history);
+  };
+
+  onCheck = e => {
+    this.setState({
+      disabled: !this.state.disabled,
+      current: !this.state.current
+    });
+  };
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <Link to="/dashbaord" className="btn btn-light">
+                {" "}
+                Go Back
+              </Link>
+              <h1 className="display-4 text-center">Add Education</h1>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="School"
+                  name="school"
+                  value={this.state.school}
+                  error={errors.school}
+                />
+                <TextFieldGroup
+                  placeholder="Degree or Certification"
+                  name="degree"
+                  value={this.state.degree}
+                  error={errors.degree}
+                />
+                <TextFieldGroup
+                  placeholder="Field of Study"
+                  name="fieldofstudy"
+                  value={this.state.fieldofstudy}
+                  error={errors.fieldofstudy}
+                />
+                <TextFieldGroup
+                  placeholder="Start Date"
+                  name="startdate"
+                  value={this.state.startdate}
+                  error={errors.startdate}
+                  type="date"
+                />
+                <TextFieldGroup
+                  placeholder="End Date"
+                  name="enddate"
+                  value={this.state.enddate}
+                  error={errors.enddate}
+                  disabled={this.state.disabled ? "disabled" : ""}
+                />
+                <div className="form-check mb-4">
+                  <input
+                    name="current"
+                    value={this.state.current}
+                    checked={this.state.current}
+                    type="checkbox"
+                    className="form-check-input"
+                    onChange={this.onCheck}
+                    id="current"
+                  />
+                  <label htmlFor="current" className="form-check-label">
+                    Current Job
+                  </label>
+                </div>
+                <TextAreaFieldGroup
+                  placeholder="Program Description"
+                  name="description"
+                  value={this.state.description}
+                  onChange={this.onChange}
+                  error={errors.description}
+                />
+                <input type="submit" value="Submit" className="btn btn-block" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AddEducation.propTypes = {
+  addEducation: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { addEducation }
+)(withRouter(AddEducation));
